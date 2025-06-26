aecbd455f158e11d039154fd2aac72b5086cf438
yannbcf <yannbeats.pro@gmail.com>
2025-06-26 21:03:28 +0300

    feat: adjust hero section size and margins

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -3,29 +3,28 @@ import { Icon } from "@iconify/vue";
 </script>
 
 <template>
-  <div class="text-center max-w-6xl mx-auto mt-20 mb-16">
-    <h1 class="text-4xl sm:text-6xl md:text-8xl font-bold mb-6">
+  <div class="text-center max-w-4xl mx-auto mt-20 mb-16">
+    <h1 class="text-3xl sm:text-4xl md:text-6xl font-bold mb-6">
       Yet Another
       <span class="text-purple-400">M*</span>
       Platform.
     </h1>
-    <p class="text-white/60 text-base sm:text-lg mb-10">
+    <p class="text-white/60 text-base sm:text-lg mb-18">
       The competitive GTA V multiplayer client — scalable, fast, and built for
       performance.
     </p>
 
-    <div class="flex flex-col mb-2 gap-4">
+    <div class="flex flex-col gap-4 mb-3">
       <span class="text-4xl font-bold">Coming soon</span>
-      <span class="text-sm opacity-75">In the meantime</span>
     </div>
-    <div class="flex flex-col sm:flex-row items-center justify-center gap-4">
+    <div class="mt-8 flex flex-col sm:flex-row items-center justify-center gap-4">
       <a
         href="https://discord.gg/yamp"
         target="_blank"
         class="px-8 py-4 bg-purple-400 rounded-lg font-semibold flex items-center space-x-3 hover:opacity-90 transition-opacity text-neutral-900"
       >
-        <span>Follow us on Discord</span>
-        <Icon icon="simple-icons:discord" width="20" />
+        <span class="text-white">Follow us on Discord</span>
+        <Icon class="text-white mt-1" icon="simple-icons:discord" width="20" />
       </a>
 
       <!-- <button
