d47739aa28c22dc1f4b1cac722e30c5a80786679
yannbcf <yannbeats.pro@gmail.com>
2025-06-26 21:02:45 +0300

    feat: update stats

--- src/components/StatsSection.vue
+++ src/components/StatsSection.vue
@@ -3,16 +3,16 @@
     class="grid grid-cols-1 sm:grid-cols-3 gap-6 sm:gap-12 md:gap-24 text-center mb-20"
   >
     <div>
-      <div class="text-3xl sm:text-4xl font-bold mb-2">0</div>
-      <div class="text-white/60">Active Players</div>
-    </div>
-    <div>
-      <div class="text-3xl sm:text-4xl font-bold mb-2">0</div>
-      <div class="text-white/60">Servers</div>
-    </div>
-    <div>
       <div class="text-3xl sm:text-4xl font-bold mb-2">2642</div>
       <div class="text-white/60">Members on discord</div>
+    </div>
+    <div>
+      <div class="text-3xl sm:text-4xl font-bold mb-2">pre alpha</div>
+      <div class="text-white/60">Current phase</div>
+    </div>
+    <div>
+      <div class="text-3xl sm:text-4xl font-bold mb-2">js / lua</div>
+      <div class="text-white/60">Supported languages</div>
     </div>
   </div>
 </template>
