df11facf6838aa2967ed997beca73440d59ad887
Yiin <stanislovas.janonis@gmail.com>
2025-06-28 02:26:56 +0300

    fix: build, now for real

--- src/components/StatsSection.vue
+++ src/components/StatsSection.vue
@@ -1,31 +1,26 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const discordMembers = ref(2642);
+</script>
+
 <template>
-  <div class="grid grid-cols-1 sm:grid-cols-3 gap-6 sm:gap-12 md:gap-24 text-center mb-20">
+  <div
+    class="grid grid-cols-1 sm:grid-cols-3 gap-6 sm:gap-12 md:gap-24 text-center mb-20"
+  >
     <div>
-      <div class="text-3xl sm:text-4xl font-bold mb-2">{{ discordMembers }}</div>
+      <div class="text-3xl sm:text-4xl font-bold mb-2">
+        {{ discordMembers }}
+      </div>
       <div class="text-white/60">Members on Discord</div>
     </div>
     <div>
-      <div class="text-3xl sm:text-4xl font-bold mb-2">{{ activeServers }}</div>
-      <div class="text-white/60">Active Servers</div>
+      <div class="text-3xl sm:text-4xl font-bold mb-2">Pre-Alpha</div>
+      <div class="text-white/60">Current Phase</div>
     </div>
     <div>
-      <div class="text-3xl sm:text-4xl font-bold mb-2">{{ activePlayers }}</div>
-      <div class="text-white/60">Players In Game</div>
+      <div class="text-3xl sm:text-4xl font-bold mb-2">js / lua</div>
+      <div class="text-white/60">Supported Languages</div>
     </div>
   </div>
 </template>
-
-<script setup>
-import { ref, onMounted } from 'vue'
-
-const discordMembers = ref(2642)
-const activeServers = ref(5)
-const activePlayers = ref(9)
-// Simulated data cause why not 
-onMounted(() => {
-  setInterval(() => {
-    activeServers.value = Math.floor(Math.random() * 12)
-    activePlayers.value = activeServers.value * (Math.floor(Math.random() * 10) + 10)
-  }, 5000)
-})
-</script>
